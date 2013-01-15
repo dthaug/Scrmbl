856083d71564cec638cf7202800faadefa28192c
dthaug <daniel_haugland@hotmail.com>
2013-01-15 23:12:08 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrmbl::Application.config.secret_token = 'a9fd99c2b52fa421f93915736d25d4d901f8c6ea15b684df104c1d5581a086a1ddc6d9417b3d19d7c969fefdd3477ad60f7d746eb80316910c2c23beffc0856a'
