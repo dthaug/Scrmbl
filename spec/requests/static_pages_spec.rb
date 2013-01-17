781c2f51505b9bd747401a3520949f6bbdba86dd
dthaug <daniel_haugland@hotmail.com>
2013-01-17 01:06:15 +0100

    Added a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,23 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
+
  describe "Home page" do
    it "should have the h1 'ScrambleBox'" do
    visit '/static_pages/home'
    page.should have_selector('h1', :text => 'ScrambleBox')
   end
 
-  it "should have the title 'Home'" do
-   visit '/static_pages/home'
-   page.should have_selector('title',
-   :text => "ScrambleBox | Home")
-  end
+   it "should have the base title" do
+    visit '/static_pages/home'
+    page.should have_selector('title',
+    :text => "ScrambleBox")
+   end
+
+   it "should not have a custom page title" do
+    visit '/static_pages/home'
+    page.should_not have_selector('title', :text => '| Home')
+   end
   end
 
 describe "Help page" do
