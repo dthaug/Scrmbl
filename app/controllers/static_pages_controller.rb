e8044464cfc0df2f9e0cd6c6221c2143146b117f
dthaug <daniel_haugland@hotmail.com>
2013-02-13 22:22:08 +0100

    Started designing and implementing Scramblesite

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,7 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
+  def scrmble
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Scrmbl::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
+  match '/scrmble', to: 'static_pages#scrmble'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
